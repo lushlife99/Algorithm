8db8150a7294efdc37163c5dc9f6b5331d0791c6
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-07-31 22:09:30 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1292 ms, Memory: 143464 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
@@ -1,6 +1,4 @@
 import sys
-from queue import PriorityQueue
-from collections import deque
 import heapq
 
 input = sys.stdin.readline
@@ -9,25 +7,30 @@ n, m, k = map(int, input().strip().split())
 A = [[] for _ in range(n + 1)]
 D = [[] for _ in range(n + 1)]
 
-for i in range(m):
+for _ in range(m):
     a, b, c = map(int, input().strip().split())
     A[a].append((b, c))
 
+# K번째 최단 경로를 저장할 리스트 초기화
 heapq.heappush(D[1], 0)
 heapq.heappush(pq, (0, 1))
+
 while pq:
     dist, now = heapq.heappop(pq)
 
     for next, distance in A[now]:
         cost = dist + distance
-        if len(D[next]) < k:
-            heapq.heappush(pq, (cost, next))
-            heapq.heappush(D[next], -cost)
 
-        elif distance + dist < -D[next][0]:
+        # K개의 경로를 저장하지 않은 경우 추가
+        if len(D[next]) < k:
+            heapq.heappush(D[next], -cost)
             heapq.heappush(pq, (cost, next))
+
+        # 이미 K개의 경로를 저장한 경우, 가장 긴 경로보다 짧으면 교체
+        elif -D[next][0] > cost:
             heapq.heappop(D[next])
             heapq.heappush(D[next], -cost)
+            heapq.heappush(pq, (cost, next))
 
 for i in range(1, n + 1):
     if len(D[i]) == k:
