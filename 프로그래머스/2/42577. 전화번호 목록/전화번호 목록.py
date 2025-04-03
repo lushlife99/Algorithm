f67ce89edcecdb9cdc5a1c3f2ff149a2833b846d
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-04-03 17:22:23 +0900

    [level 2] Title: 전화번호 목록, Time: 106.72 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+# 시간복잡도 nlogn
+# 
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(1, len(phone_book)):
+        if phone_book[i][:len(phone_book[i-1])] == phone_book[i-1]:
+            return False
+        
+    return answer
