724d1f9c692a80037056cff58932638f5a085971
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-05-06 18:39:19 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String s) {
+        
+        if (s.length() % 2 == 1) {
+            return String.valueOf(s.charAt(s.length() / 2));
+        }
+        
+        return s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+    }
+}
