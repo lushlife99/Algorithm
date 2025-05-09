ca3e383f02031d4a75f428958c18df7f186c1052
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-05-09 19:58:17 +0900

    [level 1] Title: 소수 찾기, Time: 64.76 ms, Memory: 53.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+    
+    public boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i+2) == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+ 
+}
