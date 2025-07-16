30845807af24b725314a3be84863e804288add9b
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-07-16 23:32:23 +0900

    [Gold II] Title: 공항, Time: 260 ms, Memory: 22844 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,59 @@
+import java.io.*;
+
+/**
+ * BOJ 10775
+ * 그리디? 반례 찾기
+ * 100 -> 100번에 도킹
+ * 100 -> 100번 있으니까 99번 도킹
+ * 2중 반복 시간초과
+ */
+public class Main {
+
+    static int G;
+    static int P;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        G = Integer.parseInt(br.readLine());
+        P = Integer.parseInt(br.readLine());
+        parent = new int[G + 1];
+        for (int i = 0; i <= G; i++) {
+            parent[i] = i;
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < P; i++) {
+            int g = Integer.parseInt(br.readLine());
+            int dockingGate = find(g);
+
+            if (dockingGate == 0) break;
+            union(dockingGate, dockingGate - 1);
+            result++;
+        }
+        System.out.println(result);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        parent[a] = b;
+    }
+
+    static int find(int a) {
+        int root = a;
+        while (parent[root] != root) {
+            root = parent[root];
+        }
+        
+        while (a != root) {
+            int next = parent[a];
+            parent[a] = root;
+            a = next;
+        }
+
+        return root;
+    }
+}
