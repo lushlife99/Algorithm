13067a29039c1358ca59ec2b28b6fd370eece4ea
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-08-26 16:23:48 +0900

    [Gold I] Title: 트리의 독립집합, Time: 292 ms, Memory: 23516 KB -BaekjoonHub

--- 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.java
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.java
@@ -7,7 +7,6 @@ import java.util.*;
  *
  */
 
-
 public class Main {
     static int N;
     static int[] w;
@@ -19,41 +18,44 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         N = Integer.parseInt(br.readLine());
         w = new int[N+1];
-        dp = new int[N+1][2];
         visited = new boolean[N+1];
-
+        dp = new int[N+1][2];
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 1; i <= N; i++) {
             w[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i < N+1; i++) {
             edges.add(new ArrayList<>());
         }
 
         for (int i = 0; i < N-1; i++) {
             st = new StringTokenizer(br.readLine());
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            edges.get(n1).add(n2);
-            edges.get(n2).add(n1);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            edges.get(u).add(v);
+            edges.get(v).add(u);
         }
 
         dfs(1);
         bw.write(Math.max(dp[1][0], dp[1][1]) + "\n");
 
         visited = new boolean[N+1];
-        reconstruct(1, false);
+        traceBack(1, false);
         Collections.sort(result);
-        for (int x : result) {
-            bw.write(x + " ");
+        for (int u : result) {
+            bw.write(u + " ");
         }
 
         bw.flush();
         bw.close();
         br.close();
+
     }
 
     private static void dfs(int u) {
@@ -70,18 +72,18 @@ public class Main {
         }
     }
 
-    private static void reconstruct(int u, boolean parentTaken) {
+    private static void traceBack(int u, boolean parentTaken) {
         visited[u] = true;
-
         boolean take = false;
+
         if (!parentTaken && dp[u][1] > dp[u][0]) {
-            take = true;
             result.add(u);
+            take = true;
         }
 
         for (int v : edges.get(u)) {
             if (!visited[v]) {
-                reconstruct(v, take);
+                traceBack(v, take);
             }
         }
     }
