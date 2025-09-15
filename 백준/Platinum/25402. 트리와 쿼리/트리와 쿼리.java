ab1a86504e08e4c86d4345a1cd04389ac7ee8a2f
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-09-15 16:59:29 +0900

    [Platinum V] Title: 트리와 쿼리, Time: 1296 ms, Memory: 186444 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.java
@@ -2,28 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.IntStream;
 
 /**
  * boj 25402 트리와 쿼리
  * 분리 집합
  */
 
+
 public class Main {
 
-    static int N, Q;
+    static int N, M;
     static List<Integer>[] edge;
+    static int[] treeParent;
+    static boolean[] visited;
+
+    static int[] parent;
+    static long[] cnt;
+    static int[] used;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
+        edge = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) edge[i] = new ArrayList<>();
 
-        edge = IntStream.rangeClosed(0, N)
-                .mapToObj(i -> new ArrayList<>())
-                .toArray(List[]::new);
-        
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
@@ -32,55 +36,68 @@ public class Main {
             edge[v].add(u);
         }
 
-        Q = Integer.parseInt(br.readLine());
+        treeParent = new int[N + 1];
+        visited = new boolean[N + 1];
+        dfs(1, -1);
 
-        for (int i = 0; i < Q; i++) {
+        parent = new int[N + 1];
+        cnt = new long[N + 1];
+        used = new int[N + 1];
+
+        M = Integer.parseInt(br.readLine());
+        for (int q = 1; q <= M; q++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int K = Integer.parseInt(st.nextToken());
 
-            int[] parent = IntStream.rangeClosed(0, N).toArray();
-
-            Set<Integer> S = new HashSet<>();
-            while (st.hasMoreTokens()) {
-                S.add(Integer.parseInt(st.nextToken()));
+            int[] nodes = new int[K];
+            for (int i = 0; i < K; i++) {
+                nodes[i] = Integer.parseInt(st.nextToken());
+                used[nodes[i]] = q;
+                parent[nodes[i]] = nodes[i];
+                cnt[nodes[i]] = 1;
             }
 
-            for (int node : S) {
-                for (int neighbor : edge[node]) {
-                    if (S.contains(neighbor)) {
-                        union(parent, node, neighbor);
-                    }
+            for (int i = 0; i < K; i++) {
+                int node = nodes[i];
+                int p = treeParent[node];
+                if (p != -1 && used[p] == q) {
+                    union(node, p);
                 }
             }
 
-            Map<Integer, Integer> componentSize = new HashMap<>();
-            for (int node : S) {
-                int root = find(parent, node);
-                componentSize.put(root, componentSize.getOrDefault(root, 0) + 1);
+            long result = 0;
+            for (int i = 0; i < K; i++) {
+                int node = nodes[i];
+                if (find(node) == node) {
+                    long c = cnt[node];
+                    result += c * (c - 1) / 2;
+                }
             }
 
-            long result = 0;
-            for (int size : componentSize.values()) {
-                result += (long) size * (size - 1) / 2;
-            }
             sb.append(result).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
     }
 
-    private static int find(int[] parent, int a) {
-        if (a == parent[a]) {
-            return a;
+    static void dfs(int u, int p) {
+        visited[u] = true;
+        treeParent[u] = p;
+        for (int v : edge[u]) {
+            if (!visited[v]) dfs(v, u);
         }
-        return parent[a] = find(parent, parent[a]);
     }
 
-    private static void union(int[] parent, int a, int b) {
-        int rootA = find(parent, a);
-        int rootB = find(parent, b);
-        if (rootA != rootB) {
-            parent[rootB] = rootA;
-        }
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return;
+        parent[a] = b;
+        cnt[b] += cnt[a];
     }
 }
