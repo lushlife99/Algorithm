705f6a5bab5dc984ea955a6133e952ee5b296e6e
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2025-10-12 16:39:36 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 23720 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int minLen = Integer.MAX_VALUE;
+        int sum = 0;
+        int left = 0;
+
+        for (int right = 0; right < N; right++) {
+            sum += arr[right];
+
+            while (sum >= S) {
+                int len = right - left + 1;
+                if (len < minLen) {
+                    minLen = len;
+                }
+                sum -= arr[left];
+                left++;
+            }
+        }
+
+        if (minLen == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(minLen);
+        }
+    }
+}
