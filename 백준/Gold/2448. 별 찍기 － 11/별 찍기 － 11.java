dcfbced52476df7020e98b250580885200789c29
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2026-01-24 15:50:30 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 372 ms, Memory: 120964 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static char[][] map;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        map = new char[N][2 * N - 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                map[i][j] = ' ';
+            }
+        }
+
+        draw(0, N - 1, N);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(map[i]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    static void draw(int r, int c, int size) {
+        if (size == 3) {
+            map[r][c] = '*';
+            map[r + 1][c - 1] = '*';
+            map[r + 1][c + 1] = '*';
+            for (int i = -2; i <= 2; i++) {
+                map[r + 2][c + i] = '*';
+            }
+            return;
+        }
+
+        int half = size / 2;
+        draw(r, c, half);
+        draw(r + half, c - half, half);
+        draw(r + half, c + half, half);
+    }
+}
